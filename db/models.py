e9fc447c7d98b6455799bd797cdf0b5616a96435
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-11-06 11:37:07 +0100

    changing bonus filed TextField for CharField with max length = 255

--- db/models.py
+++ db/models.py
@@ -8,7 +8,7 @@ class Race(models.Model):
 
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    bonus = models.TextField(blank=True)
+    bonus = models.CharField(max_length=255, blank=True)
     race = models.ForeignKey(
         Race,
         on_delete=models.CASCADE,
